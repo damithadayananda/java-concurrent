552700eb4110129bea4837142a2887ead4015156
damitha <damithadayananda@gmail.com>
2018-12-29 12:07:01 +0530

    init commit

--- src/main.java
+++ src/main.java
@@ -2,6 +2,9 @@ import synchronize.mainSynchronize;
 
 public class main {
     public static void main(String[] args){
+        /**
+         * demonstrate usage of Synchronize block
+         */
         new mainSynchronize();
     }
 }
